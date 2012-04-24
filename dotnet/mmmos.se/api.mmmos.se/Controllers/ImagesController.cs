4435485cbbc421a57c139315be1c1e7d0531ed66
Christer Ostergaard <christer@christer.dk>
2012-04-24 07:57:33 +0200

    Created stub methods on new image controller

---
+++ dotnet/mmmos.se/api.mmmos.se/Controllers/ImagesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace api.mmmos.se.Controllers
+{
+    public class ImagesController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return Json(new { imageid = Guid.NewGuid()});
+        }
+
+        [HttpPost]
+        public ActionResult RegisterForNotification(Guid imageId, string email)
+        {
+            return new EmptyResult();
+        }
+
+
+    }
+}
